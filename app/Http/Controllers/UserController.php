426080d02b1ff2ada549e4d00eee41ea997f4762
Andreagazy <andreagazyia@gmail.com>
2024-03-05 10:45:10 +0700

    Jobsheet 4 P2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,7 +37,21 @@ class UserController extends Controller
         //     'password' => Hash::make('12345')
         // ];
         // UserModel::create($data);
-        $user = UserModel::all();
+
+        // $user = UserModel::all();
+
+        // $user = UserModel::find(1);
+
+        // $user = UserModel::where('level_id',1)->first();
+
+        // $user = UserModel::firstWhere('level_id',1);
+
+        // $user = UserModel::findOr(1,['username','nama'], function(){
+        //     abort(404);
+        // });
+        $user = UserModel::findOr(20,['username','nama'], function(){
+            abort(404);
+        });
         return view('user',['data'=> $user]);
     }
 }
