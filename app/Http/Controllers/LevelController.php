c227d7ca2f0a910d73b9a21330dd4c165fcc2011
Andreagazy <andreagazyia@gmail.com>
2024-03-05 09:44:44 +0700

    Jobsheet 3 - P6

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\DB;
 class LevelController extends Controller
 {
     public function index(){
-        // DB::insert('insert into m_level(level_kode, level_nama,created_at) values(?,?,?)', ['CUS','Pelanggan',now()]);
-        // return 'Insert Data Berhasil';
+        DB::insert('insert into m_level(level_kode, level_nama,created_at) values(?,?,?)', ['CUS','Pelanggan',now()]);
+        return 'Insert Data Berhasil';
 
         // $row = DB::update('UPDATE m_level SET level_nama = ? WHERE level_kode = ?', ['Customer','CUS']);
         // return 'Update data berhasil. Jumlah data diupdate : '. $row .' baris';
@@ -17,7 +17,7 @@ class LevelController extends Controller
         // $row = DB::delete('DELETE FROM M_level WHERE level_kode = ?', ['CUS']);
         // return 'Delete data berhasil. Jumlah data dihapus : '. $row .' baris';
 
-        $data = DB::select('select * from m_level');
-        return view('level', ['data' => $data]);
+        // $data = DB::select('select * from m_level');
+        // return view('level', ['data' => $data]);
     }
 }
